504b1ce0ac0ba1947f5ca5579ed19d7f76cd088a
Luis Diogo Zambujo <ldiogoz@outlook.pt>
2017-05-28 19:00:50 +0100

    changed backend URL to AWS EC2 DNS

--- app/src/main/java/com/ipleiria/selfiechallenge/utils/Constants.java
+++ app/src/main/java/com/ipleiria/selfiechallenge/utils/Constants.java
@@ -4,6 +4,6 @@ public final class Constants {
 
     public static String CREATEDBY = "Created by: ";
     public static String API_KEY="AIzaSyDiUsDsLLNyzpQYceHA-ZsrZ2g0VXpNw88";
-    public static String BACKEND_SERVER="http://ldz.asuscomm.com:8080";
+    public static String BACKEND_SERVER="http://ec2-34-253-189-39.eu-west-1.compute.amazonaws.com:8080";
 
 }
